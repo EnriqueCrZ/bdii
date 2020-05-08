94eb042be8027a8e8e061cdeb90b2dbbd313833b
AlphaRev <enriquecrz21@gmail.com>
2020-05-08 17:18:48 -0600

    Changes

--- bd/app.js
+++ bd/app.js
@@ -7,7 +7,10 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
+var mongoose = require('mongoose');
+
 var app = express();
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -21,6 +24,22 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
+/*var Schema = mongoose.Schema;
+
+var userSchema = new Schema({
+  name: String
+}, {collection: 'users'});
+
+var userModel = mongoose.model('userModel',userSchema)
+
+/*mongoose.model('users',{
+  name: String
+});*/
+
+app.get('/get-data', function(req, res,next) {
+  res.send('Kiuvo')
+});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- bd/routes/users.js
+++ bd/routes/users.js
@@ -1,9 +1,22 @@
 var express = require('express');
 var router = express.Router();
+var mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost:27017/test')
+var Schema = mongoose.Schema;
+
+var userSchema = new Schema({
+  name: String,
+  password: String
+}, {collection: 'users'});
+
+var userModel = mongoose.model('userModel',userSchema)
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  userModel.find().then(function(doc){
+    res.render('users',{items:doc});
+  });
 });
 
 module.exports = router;
